8aa88d38ecd6119043b98856f331d77fe487c4a7
Jayden W <jaymanw2000@gmail.com>
2021-05-24 18:19:38 -0500

    leaderboard PUT method

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 const port = 8080;
 
 const MongoClient = require('mongodb').MongoClient;
@@ -11,34 +15,7 @@ const { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } = require('react-do
 const uri = `mongodb+srv://sliderDB:${process.env.mongo_password}@slider.opswz.mongodb.net/slider?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, useUnifiedTopology: true, connectTimeoutMS: 30000, keepAlive: 1 });
 
-// const main = async () => {
-//   try { 
-//     await client.connect();
-//     await listDatabases();
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error)
-
-// const listDatabases = async () => {
-//   const databasesList = await client.db().admin().listDatabases();
-//   console.log(`Databases:`);
-//   databasesList.databases.forEach(db => {
-//     console.log(`- ${db.name}`)
-//   })
-// }
-
-// client.connect(err => {
-//   const leaderboard3 = client.db("slider").collection("leaderboard3");
-//   leaderboard3.insertOne( { username: "jaymanw", score: 21.2 });
-//   client.close();
-// });
-
-
+client.connect()
 
 const getLeaderboard3 = async () => {
   try {
@@ -50,7 +27,6 @@ const getLeaderboard3 = async () => {
   } catch (err) {
     console.error(err);
   } finally {
-    await client.close();
   }
 }
 
@@ -58,7 +34,6 @@ const getLeaderboard4 = async () => {
   try {
     await client.connect();
     const leaderboard4 = client.db("slider").collection("leaderboard4");
-    // await leaderboard3.insertOne( { username: "frank", score: 130.8 } );
     const query = leaderboard4.find({}).sort({ score: 1 }).limit(3);
     const result = await query.toArray();
     console.log(result)
@@ -66,20 +41,53 @@ const getLeaderboard4 = async () => {
   } catch (err) {
     console.error(err);
   } finally {
-    await client.close();
   }
 }
 
-// getLeaderboard4();
+const setLeaderboard3 = async (username, score) => {
+  try {
+    await client.connect();
+    const leaderboard3 = client.db("slider").collection("leaderboard3");
+    await leaderboard3.insertOne( { username: username, score: score } );
+    console.log(`Insterted ${username}'s score of ${score} successfully!`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+  }
+}
+
+const setLeaderboard4 = async (username, score) => {
+  try {
+    await client.connect();
+    const leaderboard4 = client.db("slider").collection("leaderboard4");
+    await leaderboard4.insertOne( { username: username, score: score } );
+    console.log(`Insterted ${username}'s score of ${score} successfully!`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+  }
+}
 
 app.get('/leaderboard3', async (req, res) => {
     const result = await getLeaderboard3();
     res.send(result);
 })
 
+app.post('/leaderboard3', (req, res) => {
+  const { username, score } = req.body;
+  setLeaderboard3(username, score);
+  res.status(201).json({ success: true });
+})
+
 app.get('/leaderboard4', async (req, res) => {
   const result = await getLeaderboard4();
   res.send(result);
+})
+
+app.post('/leaderboard4', (req, res) => {
+  const { username, score } = req.body;
+  setLeaderboard4(username, score);
+  res.status(201).json({ success: true });
 })
 
 app.listen(port, () => {
