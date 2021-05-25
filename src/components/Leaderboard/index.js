52d36ded7f1c1599d24e5dd3de74f62c07eae9ac
Jayden W <jaymanw2000@gmail.com>
2021-05-24 23:19:08 -0500

    4x4 leaderboards init

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -6,11 +6,20 @@ import ReturnBtn from '../ReturnBtn'
 
 function Leaderboard(props) {
     const [leaderboard3, setLeaderboard3] = useState([{ username: "", score: ""}, { username: "", score: ""}, { username: "", score: ""}]);
+    const [leaderboard4, setLeaderboard4] = useState([{ username: "", score: ""}, { username: "", score: ""}, { username: "", score: ""}]);
 
     useEffect(() => {
         axios.get('/leaderboard3')
         .then((response) => {
             setLeaderboard3(response.data);
+            console.log(response.data)
+        }).catch((err) => {
+            console.log(err);
+        })
+
+        axios.get('/leaderboard4')
+        .then((response) => {
+            setLeaderboard4(response.data);
             console.log(response.data)
         }).catch((err) => {
             console.log(err);
@@ -52,9 +61,17 @@ function Leaderboard(props) {
             <div className="leaderboard-section">
                 <h2 className="leaderboard-header">4x4 Leaderboard</h2>
                 <div className="leaderboard-scoreboard">
-                    <p>🥇 JaymanW <span className="score">(81.7)</span></p>
-                    <p>🥈 Bubsw <span className="score">(95.2)</span></p>
-                    <p>🥉 Carrfam <span className="score">(124.9)</span></p>
+                {
+                        leaderboard4.map((place, i) => {
+                            if (i === 0) {
+                                return <p key={i} >🥇 {place.username} <span className="score">({place.score})</span></p>
+                            } else if (i === 1) {
+                                return <p key={i} >🥈 {place.username} <span className="score">({place.score})</span></p>
+                            } else if (i === 2) {
+                                return <p key={i} >🥉 {place.username} <span className="score">({place.score})</span></p>
+                            }
+                        })
+                    }
                 </div>
             </div>
             <ReturnBtn switchBoards={switchBoards} />
